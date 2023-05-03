2255b7554ca7c39df3a4aabd0c42ef5c39335d1c
Muryllo <muryllo.henrique@hotmail.com>
2023-05-03 15:16:11 -0300

    Adicionando tamanhos no backend

--- loja-andreia-sanity/schemas/product.js
+++ loja-andreia-sanity/schemas/product.js
@@ -1,52 +1,57 @@
 export default {
-    name: 'produto',
-    title: 'Produto',
-    type: 'document',
-    fields: [
-      {
-        name: 'imagem',
-        title: 'Imagem',
-        type: 'array',
-        of: [{ type: 'image' }],
-        options: {
-          hotspot: true,
-        }
-      },
-      { 
-        name: 'nome',
-        title: 'Nome',
-        type: 'string',
-      },
-      { 
-        name: 'identificador',
-        title: 'Identificador',
-        type: 'slug',
-        options: {
-          source: 'nome',
-          maxLength: 90,
-        }
-      },
-      { 
-        name: 'preco',
-        title: 'Preco',
-        type: 'number',
-      },
-      {
-        name: "tamanho",
-        title: "Tamanho",
-        type: "array",
-        of: [
-          {type: "PP"},
-          {type: "P"},
-          {type: "M"},
-          {type: "G"},
-          {type: "GG"}
-        ]
-      },
-      { 
-        name: 'detalhes',
-        title: 'Detalhes',
-        type: 'string',
+  name: 'produto',
+  title: 'Produto',
+  type: 'document',
+  fields: [
+    {
+      name: 'imagem',
+      title: 'Imagem',
+      type: 'array',
+      of: [{ type: 'image' }],
+      options: {
+        hotspot: true,
       }
-    ]
-  }
+    },
+    {
+      name: 'nome',
+      title: 'Nome',
+      type: 'string',
+    },
+    {
+      name: 'identificador',
+      title: 'Identificador',
+      type: 'slug',
+      options: {
+        source: 'nome',
+        maxLength: 90,
+      }
+    },
+    {
+      name: 'preco',
+      title: 'Preco',
+      type: 'number',
+    },
+    {
+      name: "tamanho",
+      title: "Tamanho",
+      type: "array",
+      of: [{
+        type: "string",
+        options: {
+          list: [
+            { title: 'PP', value: 'PP' },
+            { title: 'P', value: 'P' },
+            { title: 'M', value: 'M' },
+            { title: 'G', value: 'G' },
+            { title: 'GG', value: 'GG' },
+          ],
+        },
+      }]
+    },
+    {
+      name: 'detalhes',
+      title: 'Detalhes',
+      type: 'string',
+    }
+  ]
+}
