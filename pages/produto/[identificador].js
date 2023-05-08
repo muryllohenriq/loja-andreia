0de2c9a0addbc7ef9cb812188e61c36ebf6a3373
Muryllo <muryllo.henrique@hotmail.com>
2023-05-08 12:17:19 -0300

    Avançando na parte de tamanhos

--- pages/produto/[identificador].js
+++ pages/produto/[identificador].js
@@ -5,10 +5,15 @@ import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
 import { useStateContext } from '../../context/StateContext';
 
-const ProductDetails = ({ product, products}) => {
+import { RadioGroup } from '@headlessui/react'
+import classNames from 'classnames';
+
+const ProductDetails = ({ product, products }) => {
   const { imagem, nome, detalhes, preco, tamanho } = product;
   const [index, setIndex] = useState(0);
   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const [selectedSize, setSelectedSize] = useState(tamanho)
 
   const handleBuyNow = () => {
     onAdd(product, qty);
@@ -25,34 +30,59 @@ const ProductDetails = ({ product, products}) => {
           </div>
           <div className='small-images-container'>
             {imagem?.map((item, i) => (
-              <img 
-              key={i}
-              src={urlFor(item)}
-              className={i === index ? 'small-image selected-image' : 'small-image'}
-              onMouseEnter={() => setIndex(i)}
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
               />
             ))}
           </div>
         </div>
-        
+
         <div className='product-detail-desc'>
           <h1>{nome}</h1>
           <h4>Detalhes: </h4>
           <p>{detalhes}</p>
           <h3>Tamanhos:</h3>
-          {tamanho.map((item) => (
-            <>
-            <input type='checkbox'id={`oi-${item}`}/>
-            <label htmlFor={`oi-${item}`}>{item}</label>
-            </>
-          ))}
+          <RadioGroup value={selectedSize} onChange={setSelectedSize} className="mt-4">           
+            <div className="grid grid-cols-4 gap-1 sm:grid-cols-8 lg:grid-cols-4">
+              {tamanho.map((item) => (
+                <RadioGroup.Option
+                  key={item}
+                  value={item}
+                  disabled={!item}
+                  className={({ active }) =>
+                    classNames(
+                      active ? 'ring-2 ring-mainColor' : '',
+                      'cursor-pointer bg-white text-gray-900 shadow-sm group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 w-20 h-10'
+                    )
+                  }
+                >
+                  {({ active, checked }) => (
+                    <>
+                      <RadioGroup.Label as="span">{item}</RadioGroup.Label>
+                        <span
+                          className={classNames(
+                            active ? 'border' : 'border-2',
+                            checked ? 'border-mainColor' : 'border-gray-200',
+                            'pointer-events-none absolute -inset-px rounded-md'
+                          )}
+                          aria-hidden="true"
+                        />
+                    </>
+                  )}
+                </RadioGroup.Option>
+              ))}
+            </div>
+          </RadioGroup>
           <p className='price'>R$ {preco}</p>
           <div className='quantity'>
             <h3>Quantidade:</h3>
             <p className='quantity-desc'>
-              <span className='minus' onClick={decQty}><AiOutlineMinus/></span>
+              <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
               <span className='num'>{qty}</span>
-              <span className='plus' onClick={incQty}><AiOutlinePlus/></span>
+              <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className='buttons'>
@@ -67,14 +97,14 @@ const ProductDetails = ({ product, products}) => {
       </div>
 
       <div className='maylike-products-wrapper'>
-          <h2>Você também pode gostar</h2>
-          <div className='marquee'>
-            <div className='maylike-products-container track'>
-              {products.map((item) => (
-                <Product key={item._id} product={item} />
-              ))}
-            </div>
+        <h2>Você também pode gostar</h2>
+        <div className='marquee'>
+          <div className='maylike-products-container track'>
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
+        </div>
       </div>
     </div>
   )
@@ -102,10 +132,10 @@ export const getStaticPaths = async () => {
   }
 }
 
-export const getStaticProps = async ({ params: { identificador }}) => {
+export const getStaticProps = async ({ params: { identificador } }) => {
   const query = `*[_type == "produto" && identificador.current == '${identificador}'][0]`;
   const productsQuery = '*[_type == "produto"]';
-  
+
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: '#F49FBC'
+      }
+    },
+  },
+  plugins: [],
+}
+
