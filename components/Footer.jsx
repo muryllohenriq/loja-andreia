467ccab299dd6cab4a676168b8b92ca394519109
Muryllo <muryllo.henrique@hotmail.com>
2023-09-13 11:06:24 -0300

    Adding ig and wpp links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className='footer-container'>
       <p>2023 Andreia Duarte Modas</p>
       <p className='icons'>
-        <a href="">
+        <a href="https://www.instagram.com/andreiaduartemodas/">
           <AiFillInstagram />
         </a>
-        <a href="">
+        <a href="https://wa.me/556481057100">
           <AiOutlineWhatsApp />
         </a>
       </p>
